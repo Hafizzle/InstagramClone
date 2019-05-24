938be4900f2efd49f7e803582932c2308849c653
Hafizzle <34069775+Hafizzle@users.noreply.github.com>
2019-05-24 18:14:40 -0500

    Update

--- app/src/main/java/com/example/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/com/example/instagramclone/Home/HomeActivity.java
@@ -2,12 +2,14 @@ package com.example.instagramclone.Home;
 
 import android.content.Context;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 
 import com.example.instagramclone.R;
 import com.example.instagramclone.Utils.BottomNavigationViewHelper;
@@ -26,13 +28,21 @@ public class HomeActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: Starting.");
 
         setupBottomNavigationView();
+        setupViewPager();
     }
+
+    /**
+     * Responisble for adding the three tabs: Camera, Home, Messages.
+     */
     private void setupViewPager(){
         SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new CameraFragment());
         adapter.addFragment(new HomeFragment());
         adapter.addFragment(new MessagesFragment());
         ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
     }
 
     /**
@@ -42,6 +52,7 @@ public class HomeActivity extends AppCompatActivity {
         Log.d(TAG, "setupBottomNavigationView: BottomNavigationView setting up");
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationView);
+        //Instantiates a Listener
         BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationView);
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
